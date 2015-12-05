b45a149519e63a1656ca090f5825a31fe2ea08b3
Toshiaki Asai <toshi.alternative@gmail.com>
2015-12-06 02:23:14 +0900

    initial commit

---
+++ matsuyatter.rb
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+require 'matsuya'
+
+Plugin.create(:matsuyatter) do
+  command(:matsuyatter_post_okanic_menu,
+          name: '松屋に行く',
+          condition: lambda{ |opt| true },
+          visible: true,
+          role: :postbox) do |opt|
+    Service.primary.post(message: Matsuya.order)
+  end
+
+  command(:matsuyatter_abduction,
+          name: '松屋に拉致する',
+          condition: lambda{ |opt| opt.messages.all? &:repliable? },
+          visible: true,
+          role: :timeline) do |opt|
+    opt.messages.each do |m|
+      m.post(message: "@#{m.user.idname} #{Matsuya.order}")
+    end
+  end
+end
